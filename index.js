d46da36915fd469fb947a572e7ab25f255008b01
ankit-2946 <ankitpal2946@gmail.com>
2022-12-04 05:54:12 -0500

    added thrre file

---
+++ index.js
@@ -0,0 +1,12 @@
+let inputTextArea = document.getElementById("input-textarea");
+let charCount = document.getElementById("char-count");
+let wordCount = document.getElementById("word-count");
+
+inputTextArea.addEventListener("input", () => {
+    
+    charCount.textContent = inputTextArea.value.length;
+    let txt = inputTextArea.value.trim();
+
+    wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+
+})
